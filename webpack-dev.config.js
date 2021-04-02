e7cbef90be87db634f436e364f2bc6c16f4d08c3
Daniel Kiss <kissd621@gmail.com>
2021-04-02 21:27:32 +0200

    initial commit

---
+++ webpack-dev.config.js
@@ -0,0 +1,15 @@
+const ExtensionReloader = require("webpack-extension-reloader");
+const config = require("./webpack.config");
+
+module.exports = {
+  ...config,
+  mode: "development",
+  plugins: [
+    new ExtensionReloader({
+      reloadPage: true,
+      entries: {
+        background: "background",
+      },
+    }),
+  ],
+};
