8701e7a3d0ccf23517e784c0f9abf6dc32af7062
Daniel Kiss <kissd621@gmail.com>
2021-04-04 12:47:39 +0200

    Added message middleman

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,5 +1,19 @@
 console.log('Background script running!');
 
+// TOGGLE ACTION
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.sendMessage(tab.id, 'toggle');
+});
+
+// MESSAGE MIDDLEMAN BETWEEN POPUP AND CONTENT SCRIPTS
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, request, ({ data }) => {
+      if (data) {
+        sendResponse({ data });
+      }
+    });
+  });
+
+  return true;
 });
