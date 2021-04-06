f16c4e75f7e025f6db378ea30b95fc59855ed52f
Daniel Kiss <kissd621@gmail.com>
2021-04-06 15:37:16 +0200

    Changed environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  graphqlHttpUri: 'https://socrative-collab-api.herokuapp.com/graphql',
+  graphqlWsUri: 'wss://socrative-collab-api.herokuapp.com/graphql',
 };
